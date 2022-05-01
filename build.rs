9b354d547ca351ceeba2f5b9557d9d9ce860561f
Marco Köpcke <hello@parakoopa.de>
2022-05-02 01:13:21 +0200

    Force disable atomic_cas.

--- build.rs
+++ build.rs
@@ -22,13 +22,7 @@ fn main() {
 }
 
 fn target_has_atomic_cas(target: &str) -> bool {
-    match &target[..] {
-        "thumbv6m-none-eabi"
-        | "msp430-none-elf"
-        | "riscv32i-unknown-none-elf"
-        | "riscv32imc-unknown-none-elf" => false,
-        _ => true,
-    }
+    false
 }
 
 fn target_has_atomics(target: &str) -> bool {
